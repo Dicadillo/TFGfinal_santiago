0253f188b7eebd2bef50ef46ce5473a40b214bb0
Dicadillo <91073255+Dicadillo@users.noreply.github.com>
2023-06-19 10:15:41 +0200

    cambios home y activity

--- Allfor1/src/Controller/ReserveController.php
+++ Allfor1/src/Controller/ReserveController.php
@@ -1,21 +1,47 @@
 <?php
+// src/Controller/ReserveController.php
 
 namespace App\Controller;
 
 use App\Entity\Activity;
+use App\Entity\Order;
+use App\Form\OrderType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ReserveController extends AbstractController
 {
-    #[Route('/activity/reserve/{id}', name: 'app_reserve')]
-    public function reserve(Activity $activity): Response
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        // Aquí puedes implementar la lógica de reserva de la actividad
-        
-        return $this->render('reserve/index.html.twig', [
-            'activity' => $activity,
-        ]);
+        $this->entityManager = $entityManager;
     }
+
+    #[Route('/activity/reserve/{id}', name: 'app_reserve')]
+public function reserve(Request $request, Activity $activity): Response
+{
+    $order = new Order();
+    $order->setActivity($activity);
+
+    $form = $this->createForm(OrderType::class, $order);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $this->entityManager->persist($order);
+        $this->entityManager->flush();
+
+        // Redirigir o mostrar un mensaje de éxito, según tus necesidades
+
+        return $this->redirectToRoute('app_activity');
+    }
+
+    return $this->render('reserve/index.html.twig', [
+        'activity' => $activity,
+        'form' => $form->createView(),
+    ]);
+}
 }
